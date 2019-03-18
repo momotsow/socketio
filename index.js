f30e957939de393d841af1aacda3967b48498cf5
momotsow <kgomotsomkansi@gmail.com>
2019-03-18 08:40:47 +0200

    files for socket io

---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var http = require("http");
+
+var app = express();
+var server = http.Server(app);
+var io = require("socket.io")(server);
+var users = []; // create a list named users
+
+//link html
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html")
+});
+
+//link css
+app.get("/style/index.css", function(req, res) {
+    res.sendFile(__dirname + "/style/index.css")
+});
+
+//connect socket io to show connected
+io.on("connection", function(socket) {
+    var name = "";
+   
+    socket.on("has connected", function(username){
+        users.push(username);
+        name = username;
+        io.emit("has connected", {username: username, usersList: users});
+        
+    });
+    //connect socket io to show disconnected
+    socket.on("disconnect", function() {
+      users.splice(users.indexOf(name), 1);
+      io.emit("has disconnected", {username: name, usersList: users});
+     });
+
+     //messages
+    socket.on("new message", function(data){
+        io.emit("new message", data);
+
+});
+
+});
+
+// port
+server.listen(3333, function(){
+    console.log("Server running on port 3333");
+});
+
